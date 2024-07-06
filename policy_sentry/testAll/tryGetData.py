cff98868febd3bc7f1c71ef23eb0c97c1b28da40
Tim <nicemantim@163.com>
2024-07-05 20:12:00 -0400

    Try to use certain function

---
+++ policy_sentry/testAll/tryGetData.py
@@ -0,0 +1,6 @@
+from policy_sentry.shared.iam_data import get_service_prefix_data
+import json
+
+if __name__ == '__main__':
+    resultS3 = get_service_prefix_data("ec2")
+    print(json.dumps(resultS3, indent=4))
