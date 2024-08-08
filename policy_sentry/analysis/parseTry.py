03635c8e6c5467b7a55049f78f0d4ebe71035698
Tim <nicemantim@163.com>
2024-08-08 12:00:16 -0400

    Modified to count each ACTION CORRECTLY and can expand * in each sequence CORRECTLY

--- policy_sentry/analysis/parseTry.py
+++ policy_sentry/analysis/parseTry.py
@@ -8,17 +8,22 @@ import os
 def extract_actions(policy):
     actions = []
     if isinstance(policy, list):
+        # 如果策略是一个列表（展开后的策略），直接返回
         return policy
     elif isinstance(policy, dict):
         if 'Statement' in policy:
             for statement in policy['Statement']:
                 if 'Action' in statement:
                     action = statement['Action']
+                    action = expand(action)
+                    # print("test for action output:")
+                    # print(action)
                     if isinstance(action, str):
                         actions.append(action)
                     elif isinstance(action, list):
                         actions.extend(action)
         else:
+            # 处理可能的单个语句情况
             if 'Action' in policy:
                 action = policy['Action']
                 if isinstance(action, str):
@@ -49,24 +54,22 @@ for filename in os.listdir(policy_directory):
             try:
                 policy = json.load(file)
 
-                expanded_policy = get_expanded_policy(policy)
-                print("\nExpanded Policy:")
-                print(json.dumps(expanded_policy, indent=2))
+                # expanded_policy = get_expanded_policy(policy)
+                # print("\nExpanded Policy:")
+                # print(json.dumps(expanded_policy, indent=2))
 
-                original_actions = extract_actions(policy)
-                print("\nOriginal Actions:")
-                print(original_actions)
-
-                expanded_actions = extract_actions(expanded_policy)
-                #expanded_actions = list_to_dict(expanded_actions)
+                expanded_actions = extract_actions(policy)
                 print("\nExpanded Actions:")
                 print(expanded_actions)
 
+                # expanded_actions = extract_actions(expanded_policy)
+                #expanded_actions = list_to_dict(expanded_actions)
+                # print("\nExpanded Actions:")
+                # print(expanded_actions)
 
-                if len(expanded_actions) > len(original_actions):
-                    file_action_counts = Counter(expanded_actions)
-                else:
-                    file_action_counts = Counter(original_actions)
+                # 使用展开后的操作进行计数
+                file_action_counts = Counter(expanded_actions)
+
                 print("\nAction Counts for this file:")
                 for action, count in file_action_counts.items():
                     print(f"{action}: {count}")
